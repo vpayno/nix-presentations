69e2701a8e59c4656efb45a9ec841ff0d7918596
Victor Payno <vpayno@users.noreply.github.com>
2025-06-29 13:45:45 -0700

    nix: set flake-utils systems to github:vpayno/nix-systems-default

--- flake.nix
+++ flake.nix
@@ -3,7 +3,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:vpayno/nix-systems-default";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
   };
 
   outputs =
@@ -11,7 +16,8 @@
       self,
       nixpkgs,
       flake-utils,
-    }:
+      ...
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
